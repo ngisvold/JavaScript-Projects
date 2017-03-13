6caee39f952defef150513a23316d8018a8a7027
Nathan <nathangisvold@gmail.com>
2017-03-13 14:25:42 -0700

    CSS Styling

--- JavaScriptFun/colorGame/script.js
+++ JavaScriptFun/colorGame/script.js
@@ -52,7 +52,7 @@ resetButton.addEventListener("click", function(){
 			//Chnage each color to match given color
 			squares[i].style.background = colors[i];
 		}
-		h1.style.background = '#232323';
+		h1.style.background = 'steelblue';
 });
 
 colorDisplay.textContent = pickedColor;
